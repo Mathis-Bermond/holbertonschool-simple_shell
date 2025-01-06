0f0896e983954d8f0ebdacfa5ac9f1bf98e81eb4
MBRorry <9985@holbertonstudents>
2025-01-06 09:45:38 +0100

    Initialize main, prompt, header, and wait source files for simple shell

---
+++ simple_shell.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <errno.h>
+
